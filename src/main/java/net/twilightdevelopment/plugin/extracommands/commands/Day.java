66ed5485d0bb07bb464cbfe18724e1e505170224
Caelum100 <caelum12321@gmail.com>
2018-07-25 18:28:20 -0600

    Optimize imports

--- src/main/java/net/twilightdevelopment/plugin/extracommands/commands/Day.java
+++ src/main/java/net/twilightdevelopment/plugin/extracommands/commands/Day.java
@@ -1,16 +1,12 @@
 package net.twilightdevelopment.plugin.extracommands.commands;
 
-import net.md_5.bungee.api.ChatColor;
 import net.twilightdevelopment.plugin.extracommands.ExtraCommand;
 import net.twilightdevelopment.plugin.extracommands.ExtraCommandExecutor;
-import net.twilightdevelopment.plugin.extracommands.placeholder.PlaceholderUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Collections;
 
 public class Day extends ExtraCommandExecutor {
 
